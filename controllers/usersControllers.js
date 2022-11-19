17f5ed45725378d6ea35ed36fdd6b7bcbcb83828
justnotiyann <muhammadfitrian0712@gmail.com>
2022-11-20 00:31:48 +0700

    add multer untuk handle data file

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,9 @@
 const Users = require("../models/Users");
+const sizeOf = require("image-size");
 
 const getUsers = async (req, res, next) => {
   try {
-    const result = await Users.find().select("_id name nim url");
+    const result = await Users.find().select("_id name nim url").lean().exec();
     res.json({
       msg: "here we go",
       data: result,
@@ -15,25 +16,27 @@ const getUsers = async (req, res, next) => {
 const createUser = async (req, res, next) => {
   try {
     const { name, nim } = req.body;
-    const imagePath = req.file.path;
+    const imagePath = req.body.path;
     const result = new Users({
       name,
       nim,
       url: imagePath,
     });
     result.save();
+
     res.json({
       msg: "berhasil membuat user",
     });
   } catch (e) {
-    next(e);
+    console.log(e);
+    // next(e);
   }
 };
 
 const getUserById = async (req, res, next) => {
   try {
     const id = req.params.id;
-    const result = await Users.findById(id).select("_id name nim url");
+    const result = await Users.findById(id).select("_id name nim url").lean().exec();
     res.json({
       msg: "berikut datanya",
       data: result,
