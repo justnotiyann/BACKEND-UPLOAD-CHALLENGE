b55ae85cb87f5d764ff963056954714b53d723f1
justnotiyann <muhammadfitrian0712@gmail.com>
2022-11-20 02:21:05 +0700

    start working with ejs

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const ejsLayout = require("express-ejs-layouts");
-
 const app = express();
 app.listen(3000, () => {
   console.log("server berjalan");
 });
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(ejsLayout);
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/users", express.static("tmp"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(ejsLayout);
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/usersRoute");
